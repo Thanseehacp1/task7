6735279dfde57c25bc616d35b673d6054c22009e
Thanseehacp1 <taanzi0001@gmail.com>
2023-12-25 13:32:54 +0530

    project init

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -3,24 +3,55 @@ import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import tab1 from '../tab1'
 import tab2 from '../tab2'
-import { useRoute } from '@react-navigation/native';
-const Tab = createBottomTabNavigator(); 
-const Home = () => {
-  
 
+
+const Home=  ({route})=>
+ {
+  return(
+  <View style={{
+    flex:1,
+   
+  }}>
+  <View style={{
+    flex:1,
+    justifyContent:"center",
+    alignSelf:"center",
+    alignContent:"center",
+    alignItems:"center",
+  }}>
+   <Text style={{
+    fontSize:20,
+    fontWeight:"bold",
+    justifyContent:"center",
+   
+   
+  }}>welcome {route.params.name}'s profile
+  </Text>
+  </View>
+  </View>
+  );
+}
+  const Tab = createBottomTabNavigator();
+  function Mytabs(){
+ 
   return (
     
     
     <Tab.Navigator
+  
     screenOptions={{
      headerShown: false,
     }}
     >
     <Tab.Screen name="Tab1" component={tab1} />
    <Tab.Screen name="Tab2" component={tab2} />
- </Tab.Navigator>
+  </Tab.Navigator>
+  
+  
   )
-}
+  
+  }
+
 
 export default Home
 
--- src/login/index.tsx
+++ src/login/index.tsx
@@ -1,13 +1,10 @@
 import { StyleSheet, Text, View ,TouchableOpacity} from 'react-native'
 import React from 'react'
-import { TextInput } from 'react-native'
+
 const Login =({navigation}) => { 
-  const [name,setName]=React.useState("");
-  const [age,setAge]=React.useState("");
+  
   return (
-    <View style={{
-      flex:1,
-    }}>
+    
       <View style={{
         flex:1,
       justifyContent:"center",
@@ -16,59 +13,12 @@ const Login =({navigation}) => {
       alignItems:"center",
       marginTop:70,
     }}>  
-    <TextInput
-    onChangeText={(text) => {
-       setName(text);
-     }}
-     value={name}
-     secureTextEntry={true}
-
-     style={{
-       backgroundColor: "#e9e9e9",
-       height: 50,
-       width: 250,
-
-       borderWidth: 1,
-       borderColor: "white",
-       paddingVertical:10,
-       paddingHorizontal:10,
-      
-       
-     }}
-     placeholder=" Enter your Name"
-   /> 
-    <TextInput
-    onChangeText={(text) => {
-       setAge(text);
-     }}
-     value={age}
-     
-
-     style={{
-       backgroundColor: "#e9e9e9",
-       height: 50,
-       width: 250,
-
-       borderWidth: 1,
-       borderColor: "white",
-       
-       paddingHorizontal:10,
-      
-       
-     }}
-     placeholder=" Enter your Age"
-   /> 
-  <View style={{
-    flex:2,
-    //justifyContent:"center",
-    marginVertical:30,
-  }}>
- 
+   
     <TouchableOpacity
-    onPress={()=>{
-      navigation.navigate("home")
+    onPress={()=>
+      navigation.navigate('home',{name:'THANSI'})
        
-    }}
+    }
     style={{
       height:40,
       width:100,
@@ -86,8 +36,9 @@ const Login =({navigation}) => {
 }}>Login</Text>
     </TouchableOpacity>
     </View>
-     </View>
-     </View>
+   
+    
+     
   )
 }
 
